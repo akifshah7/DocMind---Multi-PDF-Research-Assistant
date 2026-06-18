f2eb3b32cfa35f9ec2797108d1dccb02cf61a84f
Akif <akifshah7@gmail.com>
2026-06-18 13:20:53 +0530

    fixes

--- server.py
+++ server.py
@@ -15,6 +15,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://doc-mind-multi-pdf-research-assista.vercel.app",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
     ],
     allow_credentials=True,
     allow_methods=["*"],
